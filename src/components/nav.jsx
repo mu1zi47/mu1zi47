83a8d419cd416a12062b8f0548a80272f683da92
Муиз Валиев <valiyevmuiz0407@gmail.com>
2025-07-16 11:26:39 +0500

    fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -38,7 +38,7 @@ export default function Nav() {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: 10 }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.1 }}
             className={styles.socialText}>
             {socialButton === 1 ? (
               <Link href="https://t.me/mu1zi47" target="_blank">
